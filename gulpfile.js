442ce045ea0ff23bc833dbdc5a437c137d17e689
Rexford <seanmavley@gmail.com>
2016-10-23 08:52:44 +0000

    js minification too

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var rename = require('gulp-rename');
 var cleanCSS = require('gulp-clean-css');
 var addsrc = require('gulp-add-src');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 var sassPaths = [
   'bower_components/foundation-sites/scss',
   'bower_components/motion-ui/src'
@@ -35,13 +36,13 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('css'))
 });
 
-gulp.task('build', ['sass'], function() {
-  return gulp.src(['css/*.css'])
-    .pipe(concat('app.css'))
-    .pipe(gulp.dest('css'))
-    .pipe(rename('app.min.css'))
-    .pipe(cleanCSS({ compatibility: 'ie8' }))
-    .pipe(gulp.dest('css'));
+gulp.task('scripts', function() {
+  return gulp.src(['js/app.js', 'js/myjs.js'])
+    .pipe(concat('build.js'))
+    .pipe(gulp.dest('js'))
+    .pipe(rename('build.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('js'));
 });
 
-gulp.task('default', ['serve', 'sass']);
+gulp.task('default', ['serve', 'sass', 'scripts']);
---
+++ js/build.js
@@ -0,0 +1,67 @@
+$(document).foundation();
+
+// Code below adapted from: http://codepen.io/greenido/pen/qOEbGp
+//
+// Check if we can get geo location and show it on a map in case we can.
+//
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+    var status = document.getElementById("locationError");
+    status.style.display = 'block';
+  }
+}
+
+function showPosition(position) {
+  console.log('showPosition I am in action');
+  var geoPoint = position.coords.latitude + "," + position.coords.longitude;
+  console.log(position.coords.latitude + ' ' + position.coords.longitude);
+  document.getElementById('location').value = position.coords.latitude + ', ' + position.coords.longitude;
+}
+
+// show our errors for debuging
+function showError(error) {
+  var x = document.getElementById("status");
+  x.style.color = 'red';
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      x.innerHTML = "Denied the request for Geolocation. Maybe, ask the user in a more polite way?"
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML = "Location information is unavailable.";
+      break;
+    case error.TIMEOUT:
+      x.innerHTML = "The request to get location timed out.";
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML = "An unknown error occurred :(";
+      break;
+  }
+};
+
+$(function() {
+  // implementation of disabled form fields
+  var nowTemp = new Date();
+  var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+  var start_date = $('#dateOne').fdatepicker({
+    onRender: function(date) {
+      return date.valueOf() < now.valueOf() ? 'disabled' : '';
+    }
+  }).on('changeDate', function(ev) {
+    if (ev.date.valueOf() > end_date.date.valueOf()) {
+      var newDate = new Date(ev.date)
+      newDate.setDate(newDate.getDate() + 1);
+      end_date.update(newDate);
+    }
+    start_date.hide();
+    $('#dateTwo')[0].focus();
+  }).data('datepicker');
+  var end_date = $('#dateTwo').fdatepicker({
+    onRender: function(date) {
+      return date.valueOf() <= start_date.date.valueOf() ? 'disabled' : '';
+    }
+  }).on('changeDate', function(ev) {
+    end_date.hide();
+  }).data('datepicker');
+});
